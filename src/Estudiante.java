9f7e8fba21c7c38620499086add2c6370ccfd1fb
POO <joel.torres@epn.edu.ec>
2025-05-08 09:38:59 -0500

    Creacion de clase y constructor

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,2 +1,12 @@
 public class Estudiante {
+    public String nombre;
+    public double calificacion;
+    public double promedio;
+
+    public Estudiante (String nombre, double calificacion, double promedio){
+       this.nombre=nombre;
+       this.calificacion=calificacion;
+       this.promedio=promedio;
+    }
+
 }
