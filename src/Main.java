6b7018d369fcd0fffefb04dfa3ca07b9de22691c
POO <joel.torres@epn.edu.ec>
2025-05-08 10:02:09 -0500

    lEER DATOS POR TECLADO

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int it = 0;
 
+        System.out.println("Ingrese el nombre del estudiante: ");
+        String nombreEstudiante = teclado.nextLine();
+        System.out.println("Ingrese cuántas notas va a registrar:");
+        int numeroNotas = teclado.nextInt();
+        teclado.nextLine();
 
     }
+}
