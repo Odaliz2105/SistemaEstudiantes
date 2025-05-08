37ddac473a89097a2d2d7ce0fee0fad1dc0c6608
Odaliz Aracely <balsecaodaliz@gmail.com>
2025-05-08 09:49:53 -0500

    Ingreso de notas Odaliz Balseca

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nombre del estudiante: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Ingrese la primera nota: ");
+        Double nota1 = sc.nextDouble();
+
+        System.out.print("Ingrese la segunda nota: ");
+        Double nota2 = sc.nextDouble();
+
+        System.out.print("Ingrese la tercera nota: ");
+        Double nota3 = sc.nextDouble();
+
+        //Calcular Promedio
+        double promedio=(nota1+nota2+nota3)/3;
+
+
+
 
     }
 }
